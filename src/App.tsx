1fef95b35a866f727d66572af20662203bc3e99f
Matt Corwin <corwin.matt@gmail.com>
2023-01-13 08:23:08 -0600

    Add routing and icons

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,48 @@ import {
   View,
   Card,
 } from "@aws-amplify/ui-react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import { TiktokLogo,  InstagramLogo, PinterestLogo } from "phosphor-react";
 
 function App() {
+  return (
+    <Router>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/admin" element={<Admin/>}/>
+          <Route path="/" element={<LandingPage/>}/>
+        </Routes>
+    </Router>
+  );
+}
+
+function LandingPage () {
+  return (
+    <View className="App">
+          <Card>
+            <Heading level={1}>this is the landing page</Heading>
+          </Card>
+          <Icons/>
+        </View>
+  )
+}
+
+function Icons() {
+  return (
+    <Card>
+      <PinterestLogo size={48} color="#145d44" weight="duotone" />
+      <TiktokLogo size={48} color="#145d44" weight="duotone" />
+      <InstagramLogo size={48} color="#145d44" weight="duotone" />
+    </Card>
+  )
+}
+
+function Admin() {
   return (
     <Authenticator hideSignUp={true}>
       {({ signOut, user }) => (
@@ -17,8 +57,8 @@ function App() {
           <Card>
             <Image src={logo} className="App-logo" alt="logo" />
             <Heading level={1}>You are now signed in! Good job you.</Heading>
+            <Button onClick={signOut}>Sign Out</Button>
           </Card>
-          <Button onClick={signOut}>Sign Out</Button>
         </View>
       )}
     </Authenticator>
