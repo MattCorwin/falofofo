13d4138a0f4d2ede171eeccab5b1423340c770ba
Matt Corwin <corwin.matt@gmail.com>
2023-01-14 05:15:22 -0600

    Tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -7,46 +7,75 @@ import {
   Image,
   View,
   Card,
+  Flex,
 } from "@aws-amplify/ui-react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-import { TiktokLogo,  InstagramLogo, PinterestLogo } from "phosphor-react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { TiktokLogo, InstagramLogo, PinterestLogo } from "phosphor-react";
+import { Backpack, KawaiiMood } from "react-kawaii";
+import { useState, useEffect } from "react";
 
 function App() {
   return (
     <Router>
-        {/* A <Switch> looks through its children <Route>s and
+      {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Routes>
-          <Route path="/admin" element={<Admin/>}/>
-          <Route path="/" element={<LandingPage/>}/>
-        </Routes>
+      <Routes>
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
     </Router>
   );
 }
 
-function LandingPage () {
+function LandingPage() {
+  const [mood, setMood] = useState<KawaiiMood>("blissful");
+
+  useEffect(() => {
+    const moods: KawaiiMood[] = [
+      "blissful",
+      "excited",
+      "happy",
+      "ko",
+      "lovestruck",
+      "sad",
+      "shocked",
+    ];
+    const updateMood = () => {
+      const currentIdx = moods.findIndex((listMood) => listMood === mood);
+      const nextIdx = currentIdx + 1 === moods.length ? 0 : currentIdx + 1;
+      setMood(moods[nextIdx]);
+    };
+    const timer = setTimeout(updateMood, 1000);
+    return () => clearTimeout(timer);
+  }, [mood]);
+
   return (
-    <View className="App">
-          <Card>
+    <Flex justifyContent={"center"}>
+      <View className="App">
+        <Card>
+          <Flex justifyContent={"center"}>
             <Heading level={1}>this is the landing page</Heading>
-          </Card>
-          <Icons/>
-        </View>
-  )
+          </Flex>
+          <Flex justifyContent={"center"}>
+            <Backpack size={200} mood={mood} color="#FDA7DC" />
+          </Flex>
+        </Card>
+        <Icons />
+      </View>
+    </Flex>
+  );
 }
 
 function Icons() {
   return (
     <Card>
-      <PinterestLogo size={48} color="#145d44" weight="duotone" />
-      <TiktokLogo size={48} color="#145d44" weight="duotone" />
-      <InstagramLogo size={48} color="#145d44" weight="duotone" />
+      <Flex justifyContent={"center"}>
+        <PinterestLogo size={48} color="#145d44" weight="duotone" />
+        <TiktokLogo size={48} color="#145d44" weight="duotone" />
+        <InstagramLogo size={48} color="#145d44" weight="duotone" />
+      </Flex>
     </Card>
-  )
+  );
 }
 
 function Admin() {
